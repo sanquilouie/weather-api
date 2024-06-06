d57e279a747131dd9b885c1932103ff3f6db8fec
louieeeee <louiesanqui0518@gmail.com>
2024-06-06 22:49:06 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temp = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    return {"station": station,
+            "date": date,
+            "temperature": temp}
+
+if __name__ == "__main__":
+    app.run(debug=True)
